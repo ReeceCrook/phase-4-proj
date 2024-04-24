8398327cc7bf06238decca544485647f33e67994
ReeceCrook <reececrook16@gmail.com>
2024-04-24 04:21:28 -0600

    Adjusted ViewBlog and added basic css

--- client/src/components/ViewBlog.js
+++ client/src/components/ViewBlog.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import "../css/BlogList.css"
 
 function ViewBlog() {
     const { id } = useParams();
@@ -32,33 +33,35 @@ function ViewBlog() {
 
     }, [blog])
 
-    console.log("POSTS ==>", posts)
     return (
         <div>
             {blog.name ? (
-                <div className='view-blog-wrapper'>
-                    <h1><strong>Title:</strong> {blog.name}</h1>
-                    <h3><strong>Created By:</strong> {blog.owner.username}</h3>
+                <div>
+                    <h1><strong>Title:</strong> {blog.name}</h1><br />
+                    <h3><strong>Created By:</strong> {blog.owner.username}</h3><br />
                     <h4><strong>Desciption:</strong> {blog.description}</h4> <br />
-                    {posts.length > 0 ? (
-                        posts.map((post) => {
-                            return (
-                                <div key={post.id}>
-                                    <h2>TITLE!: {post.title}</h2>
-                                    <h4>{post.description}</h4>
-                                    <p>{post.content}</p>
-                                </div>
-                            )
-                        })
-                    ) : (
-                        <div>No posts Found</div>
-                    )}
+                    <div className='blogs-wrapper'>
+                        {posts.length > 0 ? (
+                            posts.map((post) => {
+                                return (
+                                    <div key={post.id} className="blogs" >
+                                        <h2>Title: {post.title}</h2>
+                                        <h4>Desciption: {post.description}</h4>
+                                        <p>{post.content}</p>
+                                    </div>
+                                )
+                            })
+                        ) : (
+                            <div>No posts Found</div>
+                        )}
+                    </div>
+
                 </div>
             ) : (
                 <div>No Blog Found</div>
-            )}
-
-        </div>
+            )
+            }
+        </div >
     )
 }
 
